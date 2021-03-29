68ca51be44c71d5d4234b089bc3a1ea7ff156aa6
scoopscode <71532026+scoopscode@users.noreply.github.com>
2021-03-28 21:58:19 -0500

    push1
    First push for hosting purposes

---
+++ main.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const {Client, Attachment, MessageEmbed} =
+require('discord.js');
+const bot = new Discord.Client();
+
+const chalk = require('chalk');
+
+const fs = require('fs')
+
+const PREFIX = '$';
+
+bot.on('ready', () =>{
+    console.log(chalk.magenta.bold('Sheesh machine online'));
+    bot.user.setActivity('In development | $', {
+        type: "PLAYING"
+    }).catch(console.log);
+
+    setInterval(() => {
+        const statuses = [
+            `Lop!`,
+            `Lmao`,
+            `Lamp moment`
+        ]
+
+        const status = statuses[Math.floor(Math.random() * 3)]
+        bot.user.setActivity(status, {
+            type: "PLAYING"
+        });
+    }, 5000)
+})
+
+//Command Handler
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+bot.events = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.log(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        console.log("Successfully loaded " + file)
+        let commandName = file.split(".")[0];
+        bot.commands.set(commandName, props);
+    });
+});
+
+bot.login(process.env.token)
