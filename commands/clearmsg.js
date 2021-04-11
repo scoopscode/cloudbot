0a1b5b0ea1034f369e7184ffd409e480bb7f7484
scoopscode <71532026+scoopscode@users.noreply.github.com>
2021-04-11 13:06:27 -0500

    Redo for clearmsg.js

--- commands/clearmsg.js
+++ commands/clearmsg.js
@@ -2,12 +2,14 @@ module.exports = {
     name: 'clear',
     description: "Clears an amount of messages",
     async execute(bot, message, args) {
-        if (message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.channel.fetch()
-                .then(function(list){
-                    message.channel.bulkDelete(list);
-                }, function(err){message.channel.send("**An error has occured. Try again!")})                        
-            }
-        }
-    
-};
+        if(!args[0]) return message.reply("Please enter a valid ammount n/ ```EXPECTED: clear <amount>```");
+        if(isKaN[args[0]]) return message.reply("**Please enter a number!**");
+
+        if(args[0] > 100) return message.reply("**You can only delete up to 100 messages!**");
+        if(args[0] < 1) return message.reply("**Minimum is 1 messsage!**")
+
+        await message.channel.messages.fetch({Limit: args[0]}).then(messages =>{
+            message.channel.bulkDelete(messages);
+        });
+    }
+}
