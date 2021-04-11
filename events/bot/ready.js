8526581804216a5c97e166454d61dacca0ec700d
scoopscode <71532026+scoopscode@users.noreply.github.com>
2021-04-11 12:35:13 -0500

    Update ready.js

--- events/bot/ready.js
+++ events/bot/ready.js
@@ -1,4 +1,7 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 const chalk = require('chalk');
+const fs = require('fs');
 
 module.exports = () =>{
     console.log(chalk.magenta.bold('Sheesh machine online'));
