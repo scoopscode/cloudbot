23f48a450e6eaa21dc145c8dfc939982592ac644
scoopscode <71532026+scoopscode@users.noreply.github.com>
2021-04-11 12:56:29 -0500

    Update clearmsg.js

--- commands/clearmsg.js
+++ commands/clearmsg.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Clears an amount of messages",
     async execute(bot, message, args) {
         if (message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.channel.fetchMessages()
+            message.channel.fetch()
                 .then(function(list){
                     message.channel.bulkDelete(list);
                 }, function(err){message.channel.send("**An error has occured. Try again!")})                        
