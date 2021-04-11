61650979e85802d115cfdb40892d29b88d91101e
scoopscode <71532026+scoopscode@users.noreply.github.com>
2021-04-11 13:13:41 -0500

    Update clearmsg.js

--- commands/clearmsg.js
+++ commands/clearmsg.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Clears an amount of messages",
     async execute(bot, message, args) {
         if(!args[0]) return message.reply("Please enter a valid ammount n/ ```EXPECTED: clear <amount>```");
-        if(isKaN[args[0]]) return message.reply("**Please enter a number!**");
+        if(isNaN[args[0]]) return message.reply("**Please enter a number!**");
 
         if(args[0] > 100) return message.reply("**You can only delete up to 100 messages!**");
         if(args[0] < 1) return message.reply("**Minimum is 1 messsage!**")
