2c5da6182fcd5c32e0a7c793d73b0673817af230
scoopscode <71532026+scoopscode@users.noreply.github.com>
2021-04-14 11:55:57 -0500

    Update ready.js

--- events/bot/ready.js
+++ events/bot/ready.js
@@ -5,4 +5,24 @@ const fs = require('fs');
 
 module.exports = () =>{
     console.log(chalk.magenta.bold('Sheesh machine online'));
+    bot.user.setActivity('Hello', {
+        type: "PLAYING"
+    }).catch(console.log);
+
+    setInterval(() => {
+        const statuses = [
+            `sup`,
+            `k`,
+            `yo mama`,
+            `lmao`,
+            `^`,
+            `lop`,
+        ]
+
+        const status = statuses[Math.floor(Math.random() * 3)]
+        bot.user.setActivity(status, {
+            type: "PLAYING"
+        });
+    }, 10000)
+})
 }
