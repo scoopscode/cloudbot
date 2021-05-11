faf99608c4511b5dac9c1ef09a707a007bafe8d1
scoopscode <71532026+scoopscode@users.noreply.github.com>
2021-05-10 21:08:36 -0500

    fix: command handler

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -4,39 +4,7 @@ module.exports = (bot, Discord) => {
     const command_files = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
     for(const file of command_files){
-        const command = require(`../commands/fun/${file}`);
-        if(command.name){
-            bot.commands.set(command.name, command);
-        } else {
-            continue;
-        }
-    }
-    for(const file of command_files){
-        const command = require(`../commands/information/${file}`);
-        if(command.name){
-            bot.commands.set(command.name, command);
-        } else {
-            continue;
-        }
-    }
-    for(const file of command_files){
-        const command = require(`../commands/misc/${file}`);
-        if(command.name){
-            bot.commands.set(command.name, command);
-        } else {
-            continue;
-        }
-    }
-    for(const file of command_files){
-        const command = require(`../commands/moderation/${file}`);
-        if(command.name){
-            bot.commands.set(command.name, command);
-        } else {
-            continue;
-        }
-    }
-    for(const file of command_files){
-        const command = require(`../commands/music/${file}`);
+        const command = require(`../commands/${dir}/${file}`);
         if(command.name){
             bot.commands.set(command.name, command);
         } else {
