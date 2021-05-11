7abced81dcbe5b63f37ac7f68754a875426bb322
scoopscode <71532026+scoopscode@users.noreply.github.com>
2021-05-10 21:00:34 -0500

    Update command_handler.js

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -4,14 +4,6 @@ module.exports = (bot, Discord) => {
     const command_files = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
     for(const file of command_files){
-        const command = require(`../commands/${file}`);
-        if(command.name){
-            bot.commands.set(command.name, command);
-        } else {
-            continue;
-        }
-    }
-    for(const file of command_files){
         const command = require(`../commands/fun/${file}`);
         if(command.name){
             bot.commands.set(command.name, command);
